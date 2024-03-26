be71d8b530fd512aa8bdca7eef0104bd035b9980
Sehktel <24711055+Sehktel@users.noreply.github.com>
2024-03-26 21:52:37 +0300

    Simple generator check
    Generator make hex from 00 --> FF six times. (Easy to divide)
    So we can check all instructions.

---
+++ bin-asm/gen1.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        generator 00-FF
+# Purpose:
+#
+# Author:      Sehktel
+#
+# Created:     26.03.2024
+# Copyright:   (c) Sehktel 2024
+# Licence:     MIT
+#-------------------------------------------------------------------------------
+
+def main():
+
+    for i in range(256):
+        print(f"{i:02X}",end="")
+        print(f"{i:02X}",end="")
+        print(f"{i:02X}",end="")
+        #print()
+    pass
+
+if __name__ == '__main__':
+    main()
